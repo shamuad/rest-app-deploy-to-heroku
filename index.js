6550360526edb7b4011fa6752e02a0608e8a5b9a
ARI DINAR <dinarari@gmail.com>
2019-02-05 17:13:25 +0100

    rest app heroku deployment

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-const sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres', { define: { timestamps: false } })
+const Sequelize = require('sequelize')
+
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+
+const sequelize = new Sequelize(connectionString, {define: { timestamps: false }})
 
 const port = 4000
 
